2424ee4b4166fed65b1530e0db27080bc5a48688
CarenStridberg <2861313096@qq.com>
2025-09-01 21:34:44 +0800

    Minor change.

---
+++ B42ModTrans_CN_As1/42.0/media/lua/client/ISWidgetRecipeCategories_CN.lua
@@ -0,0 +1,12 @@
+local _populateCategoryList = ISWidgetRecipeCategories.populateCategoryList
+
+function ISWidgetRecipeCategories:populateCategoryList()
+    _populateCategoryList(self)
+    for _, item in ipairs(self.recipeCategoryPanel.items) do
+        if item.text == "-- ALL --" then
+            item.text = getTextOrNull("UI_CraftCat_All") or item.text
+        else
+            item.text = getTextOrNull("UI_CraftCat_" .. item.text) or item.text
+        end
+    end
+end
